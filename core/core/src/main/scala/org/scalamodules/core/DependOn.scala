c0d2951903378681379f832b1eacf66de5872d6d
Heiko Seeberger <heiko.git@seebergers.de>
2009-03-12 18:07:08 +0100

    ISSUE_0018: Register services under more than one interface Providing up to three service interfaces now. Adjusted the integration tests. Demo still needs to be updated.

--- core/core/src/main/scala/org/scalamodules/core/DependOn.scala
+++ core/core/src/main/scala/org/scalamodules/core/DependOn.scala
@@ -18,22 +18,22 @@ package org.scalamodules.core
 import scala.collection.Map
 import org.osgi.framework.{BundleContext, ServiceReference, ServiceRegistration}
 import org.osgi.util.tracker.ServiceTracker
-import org.scalamodules.core._
 import org.scalamodules.core.RichBundleContext.fromBundleContext
 import org.scalamodules.core.RichServiceReference.fromServiceReference
 import org.scalamodules.util.jcl.Conversions.mapToJavaDictionary
 
 /**
- * Provides declaring a dependency for a service to be registered.
+ * Declares a dependency for a service to be registered 
+ * with one service interface. 
  */
 class DependOn[T, S](context: BundleContext,
-                     serviceInterface: Class[T],
-                     properties: Map[String, Any],
-                     dependeeInterface: Class[S]) {
+                     dependee: Class[S],
+                     t: Class[T],
+                     properties: Map[String, Any]) {
   
   require(context != null, "Bundle context must not be null!")
-  require(serviceInterface != null, "Service interface must not be null!")
-  require(dependeeInterface != null, "Dependee interface must not be null!")
+  require(dependee != null, "Dependee must not be null!")
+  require(t != null, "Service interface must not be null!")
 
   /**
    * Registers the service created by the given factory function which is given
@@ -41,7 +41,7 @@ class DependOn[T, S](context: BundleContext,
    */
   def theService(f: S => T) {
     require(f != null, "Factory function must not be null!")
-    new DependOnTracker {
+    new DependOnTracker(context, dependee, Array(t), properties) {
       override protected def createService(ref: ServiceReference) =
         f(context.getService(ref).asInstanceOf[S])
     }.open()
@@ -53,33 +53,122 @@ class DependOn[T, S](context: BundleContext,
    */
   def theService(f: (S, Map[String, Any]) => T) {
     require(f != null, "Factory function must not be null!")
-    new DependOnTracker {
+    new DependOnTracker(context, dependee, Array(t), properties) {
       override protected def createService(ref: ServiceReference) =
         f(context.getService(ref).asInstanceOf[S], ref.properties)
     }.open()
   }
+}
 
-  private abstract class DependOnTracker 
-      extends ServiceTracker(context, dependeeInterface.getName, null) {
+/**
+ * Declares a dependency for a service to be registered 
+ * with two service interfaces. 
+ */
+class DependOn2[T1, T2, S](context: BundleContext,
+                           dependee: Class[S],
+                           t1: Class[T1],
+                           t2: Class[T2],
+                           properties: Map[String, Any]) {
+  
+  require(context != null, "Bundle context must not be null!")
+  require(dependee != null, "Dependee must not be null!")
+  require(t1 != null, "First service interface must not be null!")
+  require(t2 != null, "Second service interface must not be null!")
 
-    override def addingService(ref: ServiceReference) = satisfied match {
-      case true  => null
-      case false => {
+  /**
+   * Registers the service created by the given factory function which is given
+   * the dependent service.
+   */
+  def theService(f: S => T1 with T2) {
+    require(f != null, "Factory function must not be null!")
+    new DependOnTracker(context, dependee, Array(t1, t2), properties) {
+      override protected def createService(ref: ServiceReference) =
+        f(context.getService(ref).asInstanceOf[S])
+    }.open()
+  }
+
+  /**
+   * Registers the service created by the given factory function which is given
+   * the dependent service and its properties.
+   */
+  def theService(f: (S, Map[String, Any]) => T1 with T2) {
+    require(f != null, "Factory function must not be null!")
+    new DependOnTracker(context, dependee, Array(t1, t2), properties) {
+      override protected def createService(ref: ServiceReference) =
+        f(context.getService(ref).asInstanceOf[S], ref.properties)
+    }.open()
+  }
+}
+
+/**
+ * Declares a dependency for a service to be registered 
+ * with three service interfaces. 
+ */
+class DependOn3[T1, T2, T3, S](context: BundleContext,
+                               dependee: Class[S],
+                               t1: Class[T1],
+                               t2: Class[T2],
+                               t3: Class[T3],
+                               properties: Map[String, Any]) {
+  
+  require(context != null, "Bundle context must not be null!")
+  require(dependee != null, "Dependee must not be null!")
+  require(t1 != null, "First service interface must not be null!")
+  require(t2 != null, "Second service interface must not be null!")
+  require(t3 != null, "Third service interface must not be null!")
+
+  /**
+   * Registers the service created by the given factory function which is given
+   * the dependent service.
+   */
+  def theService(f: S => T1 with T2 with T3) {
+    require(f != null, "Factory function must not be null!")
+    new DependOnTracker(context, dependee, Array(t1, t2, t3), properties) {
+      override protected def createService(ref: ServiceReference) =
+        f(context.getService(ref).asInstanceOf[S])
+    }.open()
+  }
+
+  /**
+   * Registers the service created by the given factory function which is given
+   * the dependent service and its properties.
+   */
+  def theService(f: (S, Map[String, Any]) => T1 with T2 with T3) {
+    require(f != null, "Factory function must not be null!")
+    new DependOnTracker(context, dependee, Array(t1, t2, t3), properties) {
+      override protected def createService(ref: ServiceReference) =
+        f(context.getService(ref).asInstanceOf[S], ref.properties)
+    }.open()
+  }
+}
+
+private abstract class DependOnTracker[S](context: BundleContext,
+                                          dependee: Class[S],
+                                          serviceInterfaces: Array[Class[_]],
+                                          properties: Map[String, Any])
+    extends ServiceTracker(context, dependee.getName, null) {
+
+  override def addingService(ref: ServiceReference): ServiceRegistration = 
+    synchronized {
+      satisfied match {
+        case true  => null
+        case false => {
         satisfied = true
-        context.registerService(serviceInterface.getName, 
+        context.registerService(serviceInterfaces map { _.getName }, 
                                 createService(ref), 
                                 properties)
+        }
       }
     }
 
-    override def removedService(ref: ServiceReference, registration: AnyRef) = {
+  override def removedService(ref: ServiceReference, registration: AnyRef) = 
+    synchronized {
       registration.asInstanceOf[ServiceRegistration].unregister()
       satisfied = false
       context.ungetService(ref)
     }
 
-    protected def createService(ref: ServiceReference): T
+  protected def createService(ref: ServiceReference): Any
 
-    @volatile private var satisfied = false
-  }
+  private var satisfied = false
 }
--- core/core/src/main/scala/org/scalamodules/core/RegisterAs.scala
+++ core/core/src/main/scala/org/scalamodules/core/RegisterAs.scala
@@ -20,40 +20,91 @@ import org.osgi.framework.{BundleContext, ServiceRegistration}
 import org.scalamodules.util.jcl.Conversions.mapToJavaDictionary
 
 /**
- * Provides service registration.
+ * Provides service registration for one service interface.
  */
-class RegisterAs[T](context: BundleContext,
-                    serviceInterface: Class[T],
-                    properties: Map[String, Any]) {
+class RegisterAs[T](val context: BundleContext,
+                    val t: Class[T])
+    extends Registerer[T] {
 
   require(context != null, "Bundle context must not be null!")
-  require(serviceInterface != null, "Service interface must not be null!")
+  require(t != null, "Service interface must not be null!")
 
-  def this(context: BundleContext, 
-           serviceInterface: Class[T]) {
-    this(context, serviceInterface, null)
-  }
+  /**
+   * Adds the given service interface.
+   */
+  def andAs[T2](t2: Class[T2]): RegisterAs2[T, T2] = 
+    new RegisterAs2[T, T2](context, t, t2)
 
   /**
    * Adds the given service properties.
    */
-  def withProperties(properties: Map[String, Any]) =
-    new RegisterAs(context, serviceInterface, properties)
+  def withProperties(props: Map[String, Any]): Registerer[T] =
+    new {
+      override protected val context = this.context
+      override protected val t = this.t
+      override protected val properties = props
+    } with Registerer[T]
 
+  override protected val properties = null
+}
+
+/**
+ * Provides service registration for two service interfaces.
+ */
+class RegisterAs2[T1, T2](protected val context: BundleContext,
+                          protected val t1: Class[T1],
+                          protected val t2: Class[T2])
+    extends Registerer2[T1, T2] {
+
+  require(context != null, "Bundle context must not be null!")
+  require(t1 != null, "First service interface must not be null!")
+  require(t2 != null, "Second service interface must not be null!")
 
   /**
-   * Provides declaring a dependency for a service to be registered.
+   * Adds the given service interface.
    */
-  def dependOn[S](dependee: Class[S]) = { 
-    require(dependee != null, "Dependee on must not be null!")
-    new DependOn(context, serviceInterface, properties, dependee)
-  }
+  def andAs[T3](t3: Class[T3]): RegisterAs3[T1, T2, T3] = 
+    new RegisterAs3[T1, T2, T3](context, t1, t2, t3)
 
   /**
-   * Registers the given service.
+   * Adds the given service properties.
    */
-  def theService(service: T) = {
-    require(service != null, "Service to be registered must not be null!")
-    context.registerService(serviceInterface.getName, service, properties)
-  }
+  def withProperties(props: Map[String, Any]): Registerer2[T1, T2] =
+    new {
+      override protected val context = this.context
+      override protected val t1 = this.t1
+      override protected val t2 = this.t2
+      override protected val properties = props
+    } with Registerer2[T1, T2]
+
+  override protected val properties = null
+}
+
+/**
+ * Provides service registration for three service interfaces.
+ */
+class RegisterAs3[T1, T2, T3](protected val context: BundleContext,
+                              protected val t1: Class[T1],
+                              protected val t2: Class[T2],
+                              protected val t3: Class[T3])
+    extends Registerer3[T1, T2, T3] {
+
+  require(context != null, "Bundle context must not be null!")
+  require(t1 != null, "First service interface must not be null!")
+  require(t2 != null, "Second service interface must not be null!")
+  require(t3 != null, "Third service interface must not be null!")
+
+  /**
+   * Adds the given service properties.
+   */
+  def withProperties(props: Map[String, Any]): Registerer3[T1, T2, T3] =
+    new {
+      override protected val context = this.context
+      override protected val t1 = this.t1
+      override protected val t2 = this.t2
+      override protected val t3 = this.t3
+      override protected val properties = props 
+    } with Registerer3[T1, T2, T3]
+
+  override protected val properties = null
 }
